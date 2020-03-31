711e54e5c9965d4d6c9f731572457689e3bd1569
Rizky Ramadhan <rizkyramadhan101299@gmail.com>
2020-03-31 20:42:25 +0700

    basic settings

---
+++ application/controllers/Auth.php
@@ -0,0 +1,15 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Auth extends CI_Controller
+{
+
+    public function login()
+    {
+        $this->load->view('login');
+    }
+
+    function lupa_password()
+    {
+    }
+}
